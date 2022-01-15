5f2cbbfa604f5ebefa26ef2192fb6848dc60eac9
Aleksey Vasyak <aleksej.vasjak@privatbank.ua>
2022-01-15 22:05:22 +0200

    good

--- HW1-3/index.js
+++ HW1-3/index.js
@@ -24,13 +24,7 @@ console.log( 'unknown' );
 
 if (val == 'a') {
     console.log('a');
-} else if (val == 'b') {
-    console.log('others');
-} else if (val == 'c') {
-    console.log('others');
-} else if (val == 'd') {
-    console.log('others');
-} else if (val == 'e') {
+} else if (val == 'b' || val == 'c' || val == 'd' || val == 'e') {
     console.log('others');
 }
 else {
