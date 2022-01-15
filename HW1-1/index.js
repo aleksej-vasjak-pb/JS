2145727ddf3db0ed7d38a8988ab3002caba6f2ed
Aleksey Vasyak <aleksej.vasjak@privatbank.ua>
2022-01-15 11:22:18 +0200

    first commit

---
+++ HW1-1/index.js
@@ -0,0 +1,15 @@
+/*####Задача 1
+
+Перепишите код, заменив оператор `if` на тернарный оператор `?`
+
+```js
+var result;
+if (a + b < 4) {
+result = true;
+} else {
+result = false;
+}
+```*/
+
+var result = (a + b < 4) ? result = true : result = false; 
+sdf
