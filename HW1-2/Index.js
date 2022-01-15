33db8df6ac4ae254e440fdd2df937fb3601bcfb9
Aleksey Vasyak <aleksej.vasjak@privatbank.ua>
2022-01-15 12:20:59 +0200

    good

---
+++ HW1-2/Index.js
@@ -0,0 +1,25 @@
+/*
+```js
+var message;
+
+if (login == 'Pitter') {
+message = 'Hi';
+} else if (login == 'Owner') {
+message = 'Hello';
+} else if (login == '') {
+message = 'unknown';
+} else {
+message = '';
+}
+```
+*/
+
+
+var message = (login == 'Pitter') ? message = 'Hi' :
+    (login == 'Owner') ? message = 'Hello' :
+        (login == '') ? message = 'unknown' :
+            message = '';
+
+            
+
+    
