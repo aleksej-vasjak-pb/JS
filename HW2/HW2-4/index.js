49e4c502af3f90771d504e77c1381f951775a174
Aleksey Vasyak <aleksej.vasjak@privatbank.ua>
2022-01-21 21:40:20 +0200

    good

--- HW2/HW2-4/index.js
+++ HW2/HW2-4/index.js
@@ -14,27 +14,30 @@ f('1'); // Error: parameter type is not a Number
 */
 
 function f(a) {
-    if ( a >= 1 && a <= 7) {
+    if (typeof a !== 'number')
+        throw new Error('Error: parameter type is not a Number');
+    if (a >= 1 && a <= 7) {
         switch (a) {
-            case 1:a = "Воскресенье"; break;
-            case 2:a = "Понедельник"; break;
-            case 3:a = "Вторник"; break;
-            case 4:a = "Среда"; break;
-            case 5:a = "Четверг"; break;
-            case 6:a = "Пятница"; break;
-            case 7:a = "Суббота"; break;
+            case 1: b = "Воскресенье"; break;
+            case 2: b = "Понедельник"; break;
+            case 3: b = "Вторник"; break;
+            case 4: b = "Среда"; break;
+            case 5: b = "Четверг"; break;
+            case 6: b = "Пятница"; break;
+            case 7: b = "Суббота"; break;
              
         }
-        console.log(a);
-    if ( typeof a !== 'number') {
-        throw new Error('Error: parameter type is not a Number')
+        console.log(b);
     }
     else {
         throw new Error('Error: parameter should be in the range of 1 to 7');
     }
-    }
 }
-//f(1);
+
+f(1);
 f(5);
-//f(9);
-//f("asldgfjl");
+f(7);
+f("asldgf");
+f(9);
+
+
