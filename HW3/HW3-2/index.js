73ad48b93153c883e2060b03ac5aa4c0bd965355
Aleksey Vasyak <aleksej.vasjak@privatbank.ua>
2022-01-29 21:06:12 +0200

    good

---
+++ HW3/HW3-2/index.js
@@ -0,0 +1,38 @@
+// ####Задача 2
+
+// Создать имплементацию функции `filter`, логика работы такая же как и у родного метода.
+
+// Функция должна содержать проверки:
+
+// - Первый параметр обязателен и может принимать только массив
+// - Второй параметр обязателен и может принимать только функцию
+
+// ```javascript
+// const arr = [1,2,3];
+// filter(arr, function(item, i, arr) {});
+// ```
+
+const arr = [1, 2, 3, 4, 5, 4];
+
+function myFilter(arr, func) {
+    if (Array.isArray(arr) !== true) throw new Error('Тип arr не является массивом!');
+    if (typeof func !== 'function') throw new Error('Тип func не является функцией!');
+    
+    let result = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+         if (func(arr[i], i, arr)) {
+             result.push(arr[i]);
+        }
+    }
+    return result;
+}   
+
+function func(item, i, arr) {
+    return item <= 3 ? true : false;
+}
+  
+let arr2 = myFilter(arr, func);
+    console.log(arr2);
+
+
